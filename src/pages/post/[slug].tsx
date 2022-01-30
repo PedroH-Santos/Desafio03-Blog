4f7c6a49ca1603d22741399c1130ead6b5207bd5
Pedro Santos <pedro.henrique@construsitebrasil.com.br>
2022-01-30 09:33:52 -0300

    Resolucao de errors

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -7,8 +7,8 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
-import { useRouter } from 'next/dist/client/router';
-import { Head } from 'next/document';
+import { useRouter } from 'next/router';
+import  Head  from 'next/head';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { FiUser } from 'react-icons/fi';
 import { BiTimeFive } from 'react-icons/bi';
@@ -34,19 +34,16 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({post}: PostProps) {
+export default function Post({ post }: PostProps) {
   //   // TODO
 
   const router = useRouter();
-  if(router != null) {
-    if (router.isFallback) {
-      return (
-        <h1> Carregando... </h1>
-      )
-    } 
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
   }
 
 
+  const formatterDatePublication = format(new Date(post.first_publication_date), 'dd MMM yyyy', { locale: ptBR, });
   const totalTimeForLoad = post.data.content.reduce((total, contentItem) => {
     total += contentItem.heading.split(' ').length;
     const words = contentItem.body.map(item => item.text.split(' ').length);
@@ -57,13 +54,16 @@ export default function Post({post}: PostProps) {
   const readTime = Math.ceil(totalTimeForLoad / 200);
   return (
     <>
+      <Head> 
+        <title> {`${post.data.title} | spacetraveling` } </title>
+      </Head>
       <main className={styles.container}>
         <img src={post.data.banner.url} className={styles.banner} />
         <section>
           <div className={styles.contentTop}>
             <h1> {post.data.title} </h1>
             <div className={styles.info}>
-              <time> <AiOutlineCalendar /> {post.first_publication_date} </time>
+              <time> <AiOutlineCalendar /> {formatterDatePublication} </time>
               <p> <FiUser /> {post.data.author}</p>
               <time> <BiTimeFive /> {readTime} min </time>
             </div>
@@ -107,23 +107,26 @@ export const getStaticPaths = async () => {
   }
 };
 
-export const getStaticProps = async context => {
+export const getStaticProps : GetStaticProps = async (context) => {
   const prismic = getPrismicClient();
 
   let { slug } = context.params;
 
   const response = await prismic.getByUID('posts', String(slug), {});
   const publication = {
-    first_publication_date: format(new Date(response.first_publication_date), 'dd MMM yyyy.', { locale: ptBR, }),
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
       banner: {
         url: response.data.banner.url,
       },
-      author: response.data.author,
-      content: response.data.content.map((content) => { 
+      content: response.data.content.map((content) => {
         return {
-          heading: content.heading,
+          heading: content.heading, 
           body: [...content.body],
         }
       })
@@ -132,5 +135,5 @@ export const getStaticProps = async context => {
 
   }
 
-  return { props: { publication}  }
+  return { props: { post: publication } }
 };
